6d8c768ba98a39618b01aa77b98eb92e3168d000
Pedro Marques <pedrokanagusto@hotmail.com>
2021-09-12 10:47:00 -0300

    Initial Commit

---
+++ Viagens/ViagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViagensApp.swift
+//  Viagens
+//
+//  Created by Pedro Kanagusto on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
