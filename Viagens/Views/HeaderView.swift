3a6f041225fd9e1fe2c7a31ff5045931b4b1f991
Pedro Marques <pedrokanagusto@hotmail.com>
2021-09-13 11:38:47 -0300

    config Environment

--- Viagens/Views/HeaderView.swift
+++ Viagens/Views/HeaderView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
         
         GeometryReader { view in
@@ -18,49 +21,53 @@ struct HeaderView: View {
             VStack {
                 Text("Swift Viagens")
                     .foregroundColor(Color.white)
-                    .font(.custom("Avenir Black", size: 20))
-                    .padding(.top, 50)
+                    .font(.custom("Avenir Black", size:
+                                    self.horizontalSizeClass == .compact ? 20 : 30))
+                    .padding(.top, self.horizontalSizeClass ==  .compact ? 50 : 90)
                 Text("Especial")
                     .foregroundColor(Color.white)
-                    .font(.custom("Avenir Book", size: 20))
+                    .font(.custom("Avenir Book", size:
+                                    self.horizontalSizeClass == .compact ? 20 : 30))
                     .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
                     .padding(.leading, 30)
                 Text("Brasil")
                     .foregroundColor(Color.white)
-                    .font(.custom("Avenir Black", size: 23))
+                    .font(.custom("Avenir Black", size:
+                                    self.horizontalSizeClass == .compact ? 23 : 33))
                     .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
                     .padding(.leading, 30)
             }
-            .frame(width: view.size.width, height: 180, alignment: .top)
+            .frame(width: view.size.width, height: self.horizontalSizeClass == .compact ? 200 : 310, alignment: .top)
             .background(Color.blue)
             
             
             HStack {
                 Button(action:{}) {
                     Text("Hoteis")
-                        .font(.custom("Avenir Medium", size: 17))
+                        .font(.custom("Avenir Medium", size:
+                                        self.horizontalSizeClass == .compact ? 17 : 24))
                         .foregroundColor(.white)
                 }
-                .frame(width: 100, height: 50)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
-                .background(Color.blue)
-                .offset(x: 50)
+                .frame(width: self.horizontalSizeClass ==  .compact ? 100 : 150, height: self.horizontalSizeClass ==  .compact ? 50 : 75)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.red, lineWidth: 10))
+                .background(Color.red)
+                .offset(x: self.horizontalSizeClass == .compact ? 50 : view.size.width/4)
                 
                 Spacer()
                 
                 Button(action:{}) {
                     Text("Pacotes")
-                        .font(.custom("Avenir Medium", size: 17))
-                        .foregroundColor(.white)
+                        .font(.custom("Avenir Medium", size:
+                                        self.horizontalSizeClass == .compact ? 17 : 24))      .foregroundColor(.white)
             
                 }
-                .frame(width: 100, height: 50)
+                .frame(width: self.horizontalSizeClass ==  .compact ? 100 : 150, height: self.horizontalSizeClass ==  .compact ? 50 : 75)
                 .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
                 .background(Color.orange)
                 .background(Color.orange)
-                .offset(x: -50)
+                .offset(x:  self.horizontalSizeClass == .compact ? -50 : -view.size.width/4)
             }
-            .offset(y: -25)
+            .offset(y: self.horizontalSizeClass ==  .compact ? -25 : -37.5)
         }
       }
     }
@@ -68,7 +75,14 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
-            .previewLayout(.fixed(width: 400, height: 220))
+        
+        Group {
+            HeaderView()
+                .environment(\.horizontalSizeClass, .compact)
+                .previewLayout(.fixed(width: 400, height: 220))
+            HeaderView()
+                .previewLayout(.fixed(width: 835, height: 310))
+        }
+      
     }
 }
