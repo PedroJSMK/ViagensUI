e039f07c1c7071b14a1371c050084bd4673b1afc
Pedro Marques <pedrokanagusto@hotmail.com>
2021-09-13 09:57:26 -0300

    Primeiro commit

--- Viagens/ContentView.swift
+++ Viagens/ContentView.swift
@@ -2,17 +2,52 @@
 //  ContentView.swift
 //  Viagens
 //
-//  Created by Pedro Kanagusto on 12/09/21.
+//  Created by PedroJSMK on 12/09/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader { view in
+            
+        VStack {
+                
+            VStack {
+                Text("Swift Viagens")
+                    .foregroundColor(Color.white)
+                    .font(.custom("Avenir Black", size: 20))
+                    .padding(.top, 50)
+                Text("Especial")
+                    .foregroundColor(Color.white)
+                    .font(.custom("Avenir Book", size: 20))
+                    .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    .padding(.leading, 30)
+                Text("Brasil")
+                    .foregroundColor(Color.white)
+                    .font(.custom("Avenir Black", size: 23))
+                    .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    .padding(.leading, 30)
+            }
+            .frame(width: view.size.width, height: 180, alignment: .top)
+            .background(Color.blue)
+            .edgesIgnoringSafeArea(.all)
+            
+            List {
+                Text("Rio de Janeiro")
+                Text("Ceará")
+                Text("Sao Paulo")
+                Text("fortaleza")
+
+            }
+        }
+    }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Viagens/ViagensApp.swift
+++ Viagens/ViagensApp.swift
@@ -2,7 +2,7 @@
 //  ViagensApp.swift
 //  Viagens
 //
-//  Created by Pedro Kanagusto on 12/09/21.
+//  Created by PedroJSMK on 12/09/21.
 //
 
 import SwiftUI
